fd567cc015ff05c949c4489cd983b247321fea22
marcingal <marcin921125@gmail.com>
2018-12-11 11:54:19 +0100

    counterReducer added

--- src/store.js
+++ src/store.js
@@ -1,25 +1,47 @@
 import { createStore, combineReducers } from 'redux'
 
+const FIRST_ACTION_INITIAL_STATE = {
+    firstAction: false,
 
-const myData = () => ({
-    type: 'FIRST_NAME',
-    name: 'marcin'
-})
+}
 
-const neighbourData = () => ({
-    type: 'FIRST_NAME',
-    name: 'zdzichu'
-})
+const COUNTER_INITIAL_STATE = {
+    counter: 0
+}
 
-const sisterData = () => ({
-    type: 'FIRST_NAME',
-    name: 'mery'
-})
+const INC = 'INC'
+const DEC = 'DEC'
+
+const counterReducerName = (state = COUNTER_INITIAL_STATE, action) => {
+    switch (action.type) {
+        case INC:
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case DEC:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        default:
+            return state
+    }
+}
+
+const firstActionReducerName = (state = FIRST_ACTION_INITIAL_STATE, aciton) => {
+    if (aciton.type === 'FIRST_ACTION') {
+        return {
+            ...state,
+            firstAction: true
+        }
+    }
+    return state
+}
 
 const reducer = combineReducers({
-    myData,
-    neighbourData,
-    sisterData
+    firstActionReducerName,
+    counterReducerName
 })
 
 export const store = createStore(
@@ -27,4 +49,28 @@ export const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-console.log(store)
+const FIRST_ACTION = 'FIRST_ACTION'
+
+const firstAction = () => ({
+    type: FIRST_ACTION
+})
+
+const incCounter = () => ({
+    type: INC
+})
+
+const decCounter = () => ({
+    type: DEC
+})
+
+
+window.dispatchFirstAction = () => {
+    store.dispatch(firstAction())
+}
+
+window.dispatchIncCounter = () => {
+    store.dispatch(incCounter())
+}
+
+window.dispatchDecCounter = () => {
+    store.dispatch(decCounter())}

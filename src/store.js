2cfc76c4ec8376ea775bb30018226ab066118865
marcingal <marcin921125@gmail.com>
2018-12-11 10:22:52 +0100

    adding random objects to store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,30 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 
-const reducer =  () => ({
+const myData = () => ({
+    type: 'FIRST_NAME',
+    name: 'marcin'
+})
 
+const neighbourData = () => ({
+    type: 'FIRST_NAME',
+    name: 'zdzichu'
+})
+
+const sisterData = () => ({
+    type: 'FIRST_NAME',
+    name: 'mery'
+})
+
+const reducer = combineReducers({
+    myData,
+    neighbourData,
+    sisterData
 })
 
 export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+)
 
-    console.log(store)
+console.log(store)
